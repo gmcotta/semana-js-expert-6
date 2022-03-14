8031db0f9a86ba8cfe8099e7027ffad6921ca8f7
Gustavo Cotta <gmcotta34@gmail.com>
2022-03-14 18:34:48 -0300

    feat: update '/' and '/home' routes

--- server/routes.js
+++ server/routes.js
@@ -1,3 +1,31 @@
-export function handler(request, response) {
+import config from './config.js';
+import { Controller } from './controller.js';
+import { logger } from './util.js';
+
+const {
+  location,
+  pages: {
+    homeHTML
+  }
+} = config;
+const controller = new Controller();
+
+async function routes(request, response) {
+  const { method, url } = request;
+  if (method === 'GET' && url === '/') {
+    response.writeHead(302, {
+      'Location': config.location.home
+    });
+    return response.end();
+  }
+  if (method === 'GET' && url === '/home') {
+    const { stream } = await controller.getFileStream(homeHTML);
+    return stream.pipe(response);
+  }
   return response.end('hello');
+}
+
+export function handler(request, response) {
+  return routes(request, response)
+    .catch(error => logger.error(`${error.stack}`));
 }
