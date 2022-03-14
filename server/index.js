a3d9b334420ac825abe44564c15f1a77e801da81
Gustavo Cotta <gmcotta34@gmail.com>
2022-03-14 17:58:03 -0300

    create config file

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 import server from './server.js';
 import { logger } from './util.js';
+import config from './config.js';
 
 server
-  .listen(3000)
-  .on('listening', () => logger.info('server running'));
+  .listen(config.port)
+  .on('listening', () => logger.info(`server running at port ${config.port}`));
 
