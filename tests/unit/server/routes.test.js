62bb7cc4c183f7b9a3c70b5221c5933dfc76b7a4
Gustavo Cotta <gmcotta34@gmail.com>
2022-03-15 14:14:57 -0300

    test: verify GET / route

--- tests/unit/server/routes.test.js
+++ tests/unit/server/routes.test.js
@@ -1,7 +1,39 @@
-import { jest, expect, describe, test } from '@jest/globals';
+import { jest, expect, describe, test, beforeEach } from '@jest/globals';
 
-describe('Routes', () => {
-  test('test', () => {
-    expect(true).toBeTruthy();
+import config from '../../../server/config.js';
+import { handler } from '../../../server/routes.js';
+import TestUtil from '../_util/testUtil.js';
+
+const { pages, location } = config;
+
+describe('#Routes - test suite for api response', () => {
+  beforeEach(() => {
+    jest.restoreAllMocks();
+    jest.clearAllMocks();
+  });
+
+  describe('GET', () => {
+    test('/ - should redirect to home page', async () => {
+      const params = TestUtil.defaultHandleParams();
+      params.request.method = 'GET';
+      params.request.url = '/';
+      await handler(...params.values());
+      expect(params.response.writeHead).toHaveBeenCalledWith(
+        302, 
+        {
+          'Location': location.home
+        }
+      );
+      expect(params.response.end).toHaveBeenCalled();
+    });
+    test.todo(`/home - should return with ${config.pages.homeHTML} file stream`);
+    test.todo(`/controller - should return with ${config.pages.controllerHTML} file stream`);
+    test.todo('/file.ext - should return with file stream');
+    test.todo('invalid route - should return 404');
+  });
+
+  describe('exceptions', () => {
+    test.todo('invalid file - should return 404');
+    test.todo('internal error - should return 500');
   });
 });
