0032adc66db1b0fd066647f79277ab5e5076890a
Gustavo Cotta <gmcotta34@gmail.com>
2022-03-15 14:32:06 -0300

    test: verify GET /home route

--- tests/unit/server/routes.test.js
+++ tests/unit/server/routes.test.js
@@ -1,6 +1,7 @@
 import { jest, expect, describe, test, beforeEach } from '@jest/globals';
 
 import config from '../../../server/config.js';
+import { Controller } from '../../../server/controller.js';
 import { handler } from '../../../server/routes.js';
 import TestUtil from '../_util/testUtil.js';
 
@@ -17,7 +18,9 @@ describe('#Routes - test suite for api response', () => {
       const params = TestUtil.defaultHandleParams();
       params.request.method = 'GET';
       params.request.url = '/';
+
       await handler(...params.values());
+      
       expect(params.response.writeHead).toHaveBeenCalledWith(
         302, 
         {
@@ -26,8 +29,25 @@ describe('#Routes - test suite for api response', () => {
       );
       expect(params.response.end).toHaveBeenCalled();
     });
-    test.todo(`/home - should return with ${config.pages.homeHTML} file stream`);
-    test.todo(`/controller - should return with ${config.pages.controllerHTML} file stream`);
+    test(`/home - should return with ${pages.homeHTML} file stream`, async () => {
+      const params = TestUtil.defaultHandleParams();
+      params.request.method = 'GET';
+      params.request.url = '/home';
+      const mockFileStream = TestUtil.generateReadableStream(['data']);
+      jest.spyOn(
+        Controller.prototype, 
+        Controller.prototype.getFileStream.name
+      ).mockResolvedValue({
+        stream: mockFileStream
+      });
+      jest.spyOn(mockFileStream, 'pipe').mockReturnValue();
+
+      await handler(...params.values());
+
+      expect(Controller.prototype.getFileStream).toBeCalledWith(pages.homeHTML);
+      expect(mockFileStream.pipe).toHaveBeenCalledWith(params.response);
+    });
+    test.todo(`/controller - should return with ${pages.controllerHTML} file stream`);
     test.todo('/file.ext - should return with file stream');
     test.todo('invalid route - should return 404');
   });
