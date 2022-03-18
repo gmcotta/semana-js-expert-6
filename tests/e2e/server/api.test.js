cefd7b8b795e39ab7f6182d40cd70437394de592
Gustavo Cotta <gmcotta34@gmail.com>
2022-03-18 12:02:35 -0300

    test: ensure client is receiving data stream if process is playing

--- tests/e2e/server/api.test.js
+++ tests/e2e/server/api.test.js
@@ -8,6 +8,14 @@ import Server from '../../../server/server.js';
 
 const getAvailablePort = portfinder.getPortPromise;
 const RETENTION_DATA_PERIOD = 200;
+const DEFAULT_COMMAND_RESPONSE = JSON.stringify({
+  result: 'ok'
+});
+const POSSIBLE_COMMANDS = {
+  start: 'start',
+  stop: 'stop',
+  invalid: 'invalid'
+};
 
 describe('API E2E Suite test', () => {
   function pipeAndReadStreamData(stream, onChunk) {
@@ -38,6 +46,19 @@ describe('API E2E Suite test', () => {
           .once('error', reject);
       })
     }
+
+    function commandSender(testServer) {
+      return {
+        async send(command) {
+          const response = await testServer.post('/controller')
+            .send({
+              command
+            });
+          expect(response.text).toStrictEqual(DEFAULT_COMMAND_RESPONSE);
+        }
+      }
+    }
+
     test('should not receive data stream if the process is not playing', async () => {
       const server = await getTestServer();
       const stream = server.testServer.get('/stream');
@@ -47,6 +68,19 @@ describe('API E2E Suite test', () => {
       server.kill();
       expect(onChunk).not.toHaveBeenCalled();
     });
-    test.todo('should receive data stream if the process is playing');
+    test('should receive data stream if the process is playing', async () => {
+      const server = await getTestServer();
+      const stream = server.testServer.get('/stream');
+      const onChunk = jest.fn();
+      const { send } = commandSender(server.testServer);
+      pipeAndReadStreamData(stream, onChunk);
+      await send(POSSIBLE_COMMANDS.start);
+      await setTimeout(RETENTION_DATA_PERIOD);
+      await send(POSSIBLE_COMMANDS.stop);
+      const [buffer] = onChunk.mock.calls[0];
+      expect(buffer).toBeInstanceOf(Buffer);
+      expect(buffer.length).toBeGreaterThan(1000);
+      server.kill();
+    });
   });
 });
