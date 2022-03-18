2e856b50389ba750c89a6a421299f6ecfd84276b
Gustavo Cotta <gmcotta34@gmail.com>
2022-03-18 17:28:56 -0300

    test: ensure _executeSoxCommand and createClientStream functions are tested

--- tests/unit/server/service.test.js
+++ tests/unit/server/service.test.js
@@ -2,10 +2,13 @@ import { jest, expect, describe, test, beforeEach } from '@jest/globals';
 import fs from 'fs';
 import fsPromises from 'fs/promises';
 import path from 'path';
+import childProcess from 'child_process';
+import crypto from 'crypto';
 
 import { Service } from '../../../server/service.js';
 import config from '../../../server/config.js';
 import TestUtil from '../_util/testUtil.js';
+import { PassThrough } from 'stream';
 
 const {
   dir: {
@@ -67,6 +70,29 @@ describe('#Service', () => {
       });
       expect(service.getFileInfo).toHaveBeenCalledWith(file);
       expect(service.createFileStream).toHaveBeenCalledWith(expectedName);
+    });
+  });
+
+  describe('_executeSoxCommand()', () => {
+    test('should call childProcess', () => {
+      const service = new Service();
+      const args = ['arg1'];
+      jest.spyOn(childProcess, childProcess.spawn.name).mockReturnValue();
+      service._executeSoxCommand(args);
+      expect(childProcess.spawn).toHaveBeenCalledWith('sox', args);
+    });
+  });
+
+  describe('createClientStream()', () => {
+    test('should create client stream', () => {
+      const service = new Service();
+      jest.spyOn(service.clientStreams, service.clientStreams.set.name)
+        .mockReturnValue();
+      const { id, clientStream } = service.createClientStream();
+
+      expect(clientStream).toBeInstanceOf(PassThrough);
+      expect(service.clientStreams.set)
+        .toHaveBeenCalledWith(id, clientStream);
     });
   });
 });
