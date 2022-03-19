5a713dbf8f7dc6c96ca3a3558229171bc1368c4b
Gustavo Cotta <gmcotta34@gmail.com>
2022-03-19 16:17:48 -0300

    feat: toggle controller buttons based on stream status

--- public/controller/js/view.js
+++ public/controller/js/view.js
@@ -2,7 +2,8 @@ export default class View {
   constructor() {
     this.btnStart = document.getElementById("start");
     this.btnStop = document.getElementById("stop");
-
+    this.buttons = () => Array.from(document.querySelectorAll('button'));
+    this.ignoreButtons = new Set(['unassigned']);
     async function onBtnClick() {}
     this.onBtnClick = onBtnClick;
   }
@@ -33,7 +34,48 @@ export default class View {
     }
   }) {
     const btnText = innerText;
-    console.log({ btnText });
     await this.onBtnClick(btnText);
+    this.toggleBtnStart();
+    this.changeCommandButtonsVisibility(false);
+    this.buttons()
+      .filter(btn => this.isNotUnassignedButton(btn))
+      .forEach(this.setupBtnAction.bind(this));
+  }
+
+  toggleBtnStart(active = true) {
+    if (active) {
+      this.btnStart.classList.add('hidden');
+      this.btnStop.classList.remove('hidden');
+      return;
+    }
+    this.btnStart.classList.remove('hidden');
+    this.btnStop.classList.add('hidden');
+  }
+
+  setupBtnAction(btn) {
+    const text = btn.innerText.toLowerCase();
+    if (text.includes('start')) return;
+    if (text.includes('stop')) {
+      btn.onclick = this.onStopBtn.bind(this);
+      return;
+    }
+  }
+
+  onStopBtn({
+    srcElement: {
+      innerText
+    }
+  }) {
+    this.toggleBtnStart(false);
+    this.changeCommandButtonsVisibility();
+    return this.onBtnClick(innerText);
+  }
+
+  isNotUnassignedButton(btn) {
+    const classes = Array.from(btn.classList);
+    const hasUnassignedButtons = !!classes.find(
+      item => this.ignoreButtons.has(item)
+    );
+    return !hasUnassignedButtons;
   }
 }
