d2b7c36ab75da7c99967c2024d801b57488a051e
Gustavo Cotta <gmcotta34@gmail.com>
2022-03-18 21:10:22 -0300

    test(e2e): ensure routes are tested

--- tests/e2e/server/api.test.js
+++ tests/e2e/server/api.test.js
@@ -1,5 +1,6 @@
 import { Transform } from 'stream';
 import { setTimeout } from 'timers/promises';
+import fs from 'fs';
 import { jest, expect, describe, test } from '@jest/globals';
 import supertest from 'supertest';
 import portfinder from 'portfinder';
@@ -11,7 +12,16 @@ const getAvailablePort = portfinder.getPortPromise;
 const RETENTION_DATA_PERIOD = 200;
 
 
-const { location: { home }} = config;
+const { 
+  location: { home },
+  dir: {
+    publicDirectory
+  },
+  pages: {
+    homeHTML,
+    controllerHTML
+  }
+} = config;
 
 describe('API E2E Suite test', () => {
   const DEFAULT_COMMAND_RESPONSE = JSON.stringify({
@@ -76,7 +86,6 @@ describe('API E2E Suite test', () => {
       server.kill();
       expect(onChunk).not.toHaveBeenCalled();
     });
-
     test('should receive data stream if the process is playing', async () => {
       const server = await getTestServer();
       const stream = server.testServer.get('/stream');
@@ -93,12 +102,70 @@ describe('API E2E Suite test', () => {
     });
   });
 
-  describe('redirects', () => {
+  describe('routes', () => {
     describe('GET', () => {
-      test('/ - should responde with home location and 302 status code', async () => {
+      test('/ - should return with home location and 302 status code', async () => {
         const response = await testServer.get('/');
         expect(response.statusCode).toBe(302);
         expect(response.headers.location).toBe(home);
+      });
+      test('/home - should get home/index.html', async () => {
+        const response = await testServer.get('/home');
+        const homeStatic = await fs.promises.readFile(
+          `${publicDirectory}/${homeHTML}`
+        );
+        expect(response.text).toBe(homeStatic.toString());
+      });
+      test('/controller - should get controller/index.html', async () => {
+        const response = await testServer.get('/controller');
+        const controllerStatic = await fs.promises.readFile(
+          `${publicDirectory}/${controllerHTML}`
+        );
+        expect(response.text).toBe(controllerStatic.toString());
+      });
+      test('/invalid - should return 404 status code', async () => {
+        const response = await testServer.get('/invalid');
+        expect(response.statusCode).toBe(404);
+      });
+    });
+  });
+
+  describe('static files', () => {
+    describe('GET', () => {
+      test('/home/index.html - should return content-type text/html', async () => {
+        const file = homeHTML;
+        const response = await testServer.get(`/${file}`);
+        const homeStatic = await fs.promises.readFile(
+          `${publicDirectory}/${homeHTML}`
+        );
+        expect(response.statusCode).toBe(200);
+        expect(response.text).toBe(homeStatic.toString());
+        expect(response.header['content-type']).toBe('text/html');
+      });
+      test('/home/css/styles.css - should return content-type text/css', async () => {
+        const file = 'home/css/styles.css';
+        const response = await testServer.get(`/${file}`);
+        const homeStatic = await fs.promises.readFile(
+          `${publicDirectory}/${file}`
+        );
+        expect(response.statusCode).toBe(200);
+        expect(response.text).toBe(homeStatic.toString());
+        expect(response.header['content-type']).toBe('text/css');
+      });
+      test('/home/js/animation.js - should return content-type text/javascript', async () => {
+        const file = 'home/js/animation.js';
+        const response = await testServer.get(`/${file}`);
+        const homeStatic = await fs.promises.readFile(
+          `${publicDirectory}/${file}`
+        );
+        expect(response.statusCode).toBe(200);
+        expect(response.text).toBe(homeStatic.toString());
+        expect(response.header['content-type']).toBe('text/javascript');
+      });
+      test('/invalid.ext - should return status 404', async () => {
+        const file = 'invalid.ext';
+        const response = await testServer.get(`/${file}`);
+        expect(response.statusCode).toBe(404);
       });
     });
   });
