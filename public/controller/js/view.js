11ea9be8b05b09fc1346e66f2f6ede8d0328a12e
Gustavo Cotta <gmcotta34@gmail.com>
2022-03-19 15:04:56 -0300

    feat(view): configure start tream button

--- public/controller/js/view.js
+++ public/controller/js/view.js
@@ -1,14 +1,39 @@
 export default class View {
+  constructor() {
+    this.btnStart = document.getElementById("start");
+    this.btnStop = document.getElementById("stop");
+
+    async function onBtnClick() {}
+    this.onBtnClick = onBtnClick;
+  }
+
   onLoad() {
     this.changeCommandButtonsVisibility();
+    this.btnStart.onclick = this.onStartClicked.bind(this);
   }
+
   changeCommandButtonsVisibility(hide = true) {
-    Array.from(document.querySelectorAll('[name="command"]'))
-      .forEach(button => {
-        const fn = hide ? 'add' : 'remove';
-        button.classList[fn]('unassigned');
-        function resetOnClick() {};
+    Array.from(document.querySelectorAll('[name="command"]')).forEach(
+      (button) => {
+        const fn = hide ? "add" : "remove";
+        button.classList[fn]("unassigned");
+        function resetOnClick() {}
         button.onclick = resetOnClick;
-      });
+      }
+    );
+  }
+
+  configureOnBtnClick(fn) {
+    this.onBtnClick = fn;
+  }
+
+  async onStartClicked({
+    srcElement: {
+      innerText
+    }
+  }) {
+    const btnText = innerText;
+    console.log({ btnText });
+    await this.onBtnClick(btnText);
   }
 }
