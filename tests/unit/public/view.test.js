e6803cdb33b6b682d0510ac1ce099d8525234d87
Gustavo Cotta <gmcotta34@gmail.com>
2022-03-20 19:08:03 -0300

    test: ensure toggleBtnStart is tested

--- tests/unit/public/view.test.js
+++ tests/unit/public/view.test.js
@@ -68,4 +68,91 @@ describe("#View - test suite for presentation layer", () => {
       expect(view.changeCommandButtonsVisibility).toHaveBeenCalled();
     });
   });
+
+  describe('configureOnBtnClick()', () => {
+    test('should assign function on this.onBtnClick', () => {
+      const view = new View();
+      const fn = jest.fn();
+      view.configureOnBtnClick(fn);
+      expect(view.onBtnClick).toStrictEqual(fn);
+    });
+  });
+
+  describe('onStartClicked()', () => {
+    test.skip('should call the functions', async () => {
+      const view = new View()
+
+      jest.spyOn(
+        view,
+        view.changeCommandButtonsVisibility.name,
+      ).mockReturnValue()
+
+      jest.spyOn(
+        view,
+        view.toggleBtnStart.name,
+      ).mockReturnValue()
+
+      jest.spyOn(
+        view,
+        view.onBtnClick.name,
+      ).mockResolvedValue()
+
+      jest.spyOn(
+        view,
+        "changeCommandBtnsVisibility",
+      ).mockReturnValue()
+
+      jest.spyOn(
+        view,
+        view.isNotUnassignedButton.name,
+      ).mockReturnValue(true)
+
+      jest.spyOn(
+        view,
+        view.setupBtnAction.name,
+      ).mockReturnValue()
+
+      const text = 'Start'
+      const btn = makeBtnElement({
+        text
+      })
+
+      jest.spyOn(
+        document,
+        "querySelectorAll",
+      ).mockReturnValueOnce([btn])
+
+
+      const eventOnClick = {
+        srcElement: btn
+      }
+
+      await view.onStartClicked(eventOnClick)
+
+      expect(view.toggleBtnStart).toHaveBeenCalled()
+      expect(view.onBtnClick).toHaveBeenCalledWith(text)
+      expect(view.changeCommandBtnsVisibility).toHaveBeenCalledWith(false)
+      expect(view.notIsUnassignedButton).toHaveBeenNthCalledWith(1, btn)
+
+      const [calls] = view.setupBtnAction.mock.calls[0]
+      expect(view.setupBtnAction).toBeCalledTimes(1)
+      expect(calls).toStrictEqual(btn)
+    });
+  });
+
+  describe('toggleBtnStart()', () => {
+    test('active = true - should hide button', () => {
+      const view = new View();
+      view.toggleBtnStart();
+      expect(view.btnStart.classList.add).toBeCalledWith('hidden');
+      expect(view.btnStop.classList.remove).toBeCalledWith('hidden');
+    });
+    
+    test('active = false - should show button', () => {
+      const view = new View();
+      view.toggleBtnStart(false);
+      expect(view.btnStart.classList.remove).toBeCalledWith('hidden');
+      expect(view.btnStop.classList.add).toBeCalledWith('hidden');
+    });
+  });
 });
