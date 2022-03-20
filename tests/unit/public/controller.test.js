b4b2e2d54feba9b2e4141c68b10e8da8273673f9
Gustavo Cotta <gmcotta34@gmail.com>
2022-03-20 17:49:20 -0300

    test: ensure onLoad is tested

--- tests/unit/public/controller.test.js
+++ tests/unit/public/controller.test.js
@@ -1,16 +1,25 @@
 import { jest, expect, describe, test, beforeEach } from "@jest/globals";
 import { JSDOM } from "jsdom";
 import Controller from "../../../public/controller/js/controller";
+import View from "../../../public/controller/js/view";
 
 describe('#Controller - test suite for controller class', () => {
-  const deps = {};
+  const deps = {
+    view: {
+      configureOnBtnClick: jest.fn(),
+      onLoad: jest.fn()
+    },
+    service: {
+      commandReceived: jest.fn()
+    }
+  };
 
   beforeEach(() => {
     jest.restoreAllMocks();
     jest.clearAllMocks();
   });
 
-  describe('initialize', () => {
+  describe('initialize()', () => {
     test('should call onLoad', () => {
       jest.spyOn(Controller.prototype, Controller.prototype.onLoad.name)
       .mockReturnValue();
@@ -19,5 +28,14 @@ describe('#Controller - test suite for controller class', () => {
       expect(Controller.prototype.onLoad).toHaveBeenCalled();
       expect(controller).toStrictEqual(new Controller(deps));
     });
-  })
+  });
+
+  describe('onLoad()', () => { 
+    test('should call view functions', () => {
+      const controller = new Controller(deps);
+      controller.onLoad();
+      expect(deps.view.configureOnBtnClick).toHaveBeenCalled();
+      expect(deps.view.onLoad).toHaveBeenCalled();
+    });
+   });
 });
